b4a80c3af498886611985d10bd4df46f5c001ae6
voyageth <voyageth@gmail.com>
2016-10-26 09:10:26 +0900

    feat: Add binaryTreeToHeapTest

--- src/test/java/tree/BinaryTreeNodeTest.java
+++ src/test/java/tree/BinaryTreeNodeTest.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import linkedlist.stack.LinkedListStack;
@@ -9,6 +11,128 @@ import linkedlist.stack.LinkedListStack;
  *         2016-10-26
  */
 public class BinaryTreeNodeTest {
+	@Test
+	public void binaryTreeToHeapTest() {
+		BinaryTreeNode<Integer> n_40 = new BinaryTreeNode<>(null, null, 40);
+		BinaryTreeNode<Integer> n_10 = new BinaryTreeNode<>(null, null, 10);
+		BinaryTreeNode<Integer> n_140 = new BinaryTreeNode<>(null, null, 140);
+		BinaryTreeNode<Integer> n_12 = new BinaryTreeNode<>(n_10, n_140, 12);
+		BinaryTreeNode<Integer> n_8 = new BinaryTreeNode<>(n_40, n_12, 8);
+		BinaryTreeNode<Integer> n_22 = new BinaryTreeNode<>(null, null, 22);
+		BinaryTreeNode<Integer> n_999 = new BinaryTreeNode<>(n_8, n_22, 999);
+
+		BinaryTreeNode<Integer> root = n_999;
+
+		BinaryTreeNode<Integer>[] array = convertTreeToArray(root);
+		printArray(array);
+
+		List<BinaryTreeNode<Integer>> list = java.util.Arrays.asList(array);
+		// asc sort
+		java.util.Collections.sort(list, new java.util.Comparator<BinaryTreeNode<Integer>>() {
+			@Override
+			public int compare(BinaryTreeNode<Integer> o1, BinaryTreeNode<Integer> o2) {
+				if (o1 == null) {
+					if (o2 == null) {
+						return 0;
+					} else {
+						return -1;
+					}
+				} else {
+					if (o2 == null) {
+						return 1;
+					} else {
+						if (o1.getValue() == null) {
+							if (o2.getValue() == null) {
+								return 0;
+							} else {
+								return -1;
+							}
+						} else {
+							if (o2.getValue() == null) {
+								return 1;
+							} else {
+								return o1.getValue() - o2.getValue();
+							}
+						}
+					}
+				}
+			}
+		});
+		array = list.toArray(array);
+
+		System.out.println("------------");
+		printArray(array);
+
+		// convert to heap
+		for (int i = 1; i <= array.length; i++) {
+			if (array[i - 1] == null) {
+				continue;
+			}
+
+			if ((2 * i) <= array.length) {
+				array[i - 1].setLeft(array[2 * i - 1]);
+			} else {
+				array[i - 1].setLeft(null);
+			}
+			if ((2 * i + 1) <= array.length) {
+				array[i - 1].setRight(array[2 * i + 1 - 1]);
+			} else {
+				array[i - 1].setRight(null);
+			}
+		}
+
+		root = array[0];
+		System.out.println("------------");
+		BinaryTreeNode.printNodePreorder(root);
+	}
+
+	private void printArray(BinaryTreeNode[] array) {
+		for (BinaryTreeNode node : array) {
+			System.out.println("node : " + node + ", value : " + node.getValue());
+		}
+	}
+
+	private static <T> BinaryTreeNode<T>[] convertTreeToArray(BinaryTreeNode<T> tree) {
+		if (tree == null) {
+			return null;
+		}
+
+		// find tree's size
+		int treeSize = getTreeSize(tree);
+		System.out.println("treeSize : " + treeSize);
+
+		// traversal
+		BinaryTreeNode[] array = new BinaryTreeNode[treeSize];
+		convertTreeToArrayInner(tree, 0, array);
+
+		return array;
+	}
+
+	private static <T> int convertTreeToArrayInner(BinaryTreeNode<T> node, int index, BinaryTreeNode[] array) {
+		if (node == null) {
+			return index;
+		}
+
+		array[index] = node;
+		index++;
+
+		if (node.getLeft() != null) {
+			index = convertTreeToArrayInner(node.getLeft(), index, array);
+		}
+		if (node.getRight() != null) {
+			index = convertTreeToArrayInner(node.getRight(), index, array);
+		}
+		return index;
+	}
+
+	private static <T> int getTreeSize(BinaryTreeNode<T> tree) {
+		if (tree == null) {
+			return 0;
+		}
+
+		return getTreeSize(tree.getLeft()) + getTreeSize(tree.getRight()) + 1;
+	}
+
 	@Test
 	public void nearestCommonAncestorTest() {
 		BinaryTreeNode<Integer> n_4 = new BinaryTreeNode<>(null, null, 4);

5be44dd85cd9bd2e10a736b973106c474553706e
voyageth <voyageth@gmail.com>
2016-10-28 07:19:13 +0900

    feat: Impl KevinBaconGame

--- src/main/java/tree/KevinBaconGame.java
+++ src/main/java/tree/KevinBaconGame.java
@@ -7,54 +7,87 @@ import java.util.*;
  *         2016-10-27
  */
 public class KevinBaconGame {
-    private Map<String, KevinBaconGameActor> actorMap = new HashMap<>();
+	private Map<String, KevinBaconGameActor> actorMap = new HashMap<>();
 
-    public int getKevinBaconNumber(String actorName, String targetActorName) {
-        if (actorName == null || targetActorName == null) {
-            return -1;
-        }
+	public int getKevinBaconNumber(String actorName, String targetActorName) {
+		if (actorName == null || targetActorName == null) {
+			return -1;
+		}
 
-        KevinBaconGameActor actor = actorMap.get(actorName);
-        KevinBaconGameActor targetActor = actorMap.get(targetActorName);
-        if (actor == null || targetActor == null) {
-            return -1;
-        }
+		KevinBaconGameActor actor = actorMap.get(actorName);
+		KevinBaconGameActor targetActor = actorMap.get(targetActorName);
+		if (actor == null || targetActor == null) {
+			return -1;
+		}
 
-        // TODO impl queue
+		if (actor.equals(targetActor)) {
+			return 0;
+		}
 
-        return -1;
-    }
+		Set<KevinBaconGameActor> alreadyCheckedActorSet = new HashSet<>();
+		Queue<KevinBaconGameActor> currentLevelQueue = new LinkedList<>();
+		Queue<KevinBaconGameActor> nextLevelQueue = new LinkedList<>();
+		currentLevelQueue.add(actor);
 
-    public void addMovieActorList(List<String> actorNameList) {
-        for (int i = 0; i < actorNameList.size(); i++) {
-            KevinBaconGameActor actor1 = getActor(actorNameList.get(i));
+		int number = 1;
+		KevinBaconGameActor currentActor = currentLevelQueue.poll();
+		while (currentActor != null) {
+			for (KevinBaconGameActor coWorker : currentActor.getCoWorkedActorSet()) {
+				if (alreadyCheckedActorSet.contains(coWorker)) {
+					continue;
+				}
 
-            for (int j = 0; j < actorNameList.size(); j++) {
-                if (i == j) {
-                    continue;
-                }
+				if (coWorker.equals(targetActor)) {
+					alreadyCheckedActorSet.add(coWorker);
+					return number;
+				} else {
+					nextLevelQueue.add(coWorker);
+				}
+			}
 
-                KevinBaconGameActor actor2 = getActor(actorNameList.get(j));
-                addCoWorkInfo(actor1, actor2);
-            }
-        }
-    }
+			currentActor = currentLevelQueue.poll();
+			if (currentActor == null) {
+				currentLevelQueue = nextLevelQueue;
+				nextLevelQueue = new LinkedList<>();
+				number++;
 
-    private KevinBaconGameActor getActor(String actor1Name) {
-        KevinBaconGameActor actor = actorMap.get(actor1Name);
-        if (actor == null) {
-            actor = new KevinBaconGameActor(actor1Name);
-            actorMap.put(actor1Name, actor);
-        }
-        return actor;
-    }
+				currentActor = currentLevelQueue.poll();
+			}
+		}
 
-    public void addCoWorkInfo(KevinBaconGameActor actor1, KevinBaconGameActor actor2) {
-        if (actor1 == null || actor2 == null) {
-            return;
-        }
+		return -1;
+	}
 
-        actor1.addCoWorker(actor2);
-        actor2.addCoWorker(actor1);
-    }
+	public void addMovieActorList(List<String> actorNameList) {
+		for (int i = 0; i < actorNameList.size(); i++) {
+			KevinBaconGameActor actor1 = getActor(actorNameList.get(i));
+
+			for (int j = 0; j < actorNameList.size(); j++) {
+				if (i == j) {
+					continue;
+				}
+
+				KevinBaconGameActor actor2 = getActor(actorNameList.get(j));
+				addCoWorkInfo(actor1, actor2);
+			}
+		}
+	}
+
+	private KevinBaconGameActor getActor(String actor1Name) {
+		KevinBaconGameActor actor = actorMap.get(actor1Name);
+		if (actor == null) {
+			actor = new KevinBaconGameActor(actor1Name);
+			actorMap.put(actor1Name, actor);
+		}
+		return actor;
+	}
+
+	public void addCoWorkInfo(KevinBaconGameActor actor1, KevinBaconGameActor actor2) {
+		if (actor1 == null || actor2 == null) {
+			return;
+		}
+
+		actor1.addCoWorker(actor2);
+		actor2.addCoWorker(actor1);
+	}
 }
--- src/test/java/tree/KevinBaconGameTest.java
+++ src/test/java/tree/KevinBaconGameTest.java
@@ -1,20 +1,20 @@
 package tree;
 
-import org.junit.Test;
-
 import java.util.Arrays;
+
+import org.junit.Test;
 
 /**
  * @author voyageth
  *         2016-10-27
  */
 public class KevinBaconGameTest {
-    @Test
-    public void test() {
-        KevinBaconGame game = new KevinBaconGame();
-        game.addMovieActorList(Arrays.asList("a", "b", "c"));
-        game.addMovieActorList(Arrays.asList("b", "c", "d"));
-        game.addMovieActorList(Arrays.asList("b", "c", "e"));
-        System.out.println(game.getKevinBaconNumber("a", "e"));
-    }
+	@Test
+	public void test() {
+		KevinBaconGame game = new KevinBaconGame();
+		game.addMovieActorList(Arrays.asList("a", "b", "c"));
+		game.addMovieActorList(Arrays.asList("b", "c", "d"));
+		game.addMovieActorList(Arrays.asList("d", "e"));
+		System.out.println(game.getKevinBaconNumber("a", "e"));
+	}
 }
